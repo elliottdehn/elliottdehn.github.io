78ebd3774a86dab08821bc4b3f30bb82a5674063
elliottdehnbostel <dehnbostele@gmail.com>
2019-11-06 21:10:41 -0600

    wrote down idioms

---
+++ build/precache-manifest.36a3aa2739379523e1eac7745d33314e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db0dca4748e3e5fbff112a9d3b6b13c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b5a2aafd10c328925a0",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "5d7f2224f0cedbb2a3af",
+    "url": "/static/js/2.25006e69.chunk.js"
+  },
+  {
+    "revision": "2b5a2aafd10c328925a0",
+    "url": "/static/js/main.4cddd557.chunk.js"
+  },
+  {
+    "revision": "b70dfea9bdde91d76b1d",
+    "url": "/static/js/runtime-main.cfd01362.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
